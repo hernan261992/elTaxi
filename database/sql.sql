63a8d5ba20978cd9a4d84f4993fcb3b5101beb81
sali3 <mlosail3@gmail.com>
2015-02-08 12:24:32 -0500

    Correccion de Estructura de tabla contactos

--- database/sql.sql
+++ database/sql.sql
@@ -3,11 +3,11 @@ CREATE DATABASE elTaxi;
 USE elTaxi;
 
 CREATE TABLE Contacto(
-  id  INT() NOT NULL AUTOINCREMENT,
+  id  INT(11) NOT NULL AUTO_INCREMENT,
   nombre VARCHAR(40) NOT NULL,
   empresa VARCHAR(50),
   telefono CHAR(12) NOT NULL,
-  e-mail  VARCHAR(60) NOT NULL,
+  e_mail  VARCHAR(60) NOT NULL,
   ciudad  VARCHAR(50) NOT NULL,
   comentario  TEXT NOT NULL,
   PRIMARY KEY Pk_contacto(id)
